744015ca532e18593f47d789c41f9012e726af3d
CINLN <1160983879@qq.com>
2025-02-08 20:56:45 +0800

    h1

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -2,10 +2,10 @@
  * @Author: Lin
  * @Description: TODO
  * @Version: v1.0.0
- * @Date: 2025/2/8 下午 8:54
+ * @Date: 2025/2/8 下午 8:54s
  **/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("123!");
     }
 }
